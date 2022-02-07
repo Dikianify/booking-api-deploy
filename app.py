5f3c6b1e6f4a254b7f5a5f58833a91379f3aa476
James Rogers <50969648+Dikianify@users.noreply.github.com>
2022-02-07 14:06:50 -0500

    debug fix 2

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ CORS(app)
 def get_times():
   date=request.get_json()[:10]
 
+  print(db.metadata.tables.keys())
   
-  raw_date_data = OpenAppointments.query.first()
+  raw_date_data = OpenAppointments.query.filter_by(date=date).first()
 
 
   date_data = []
